635c826be0552f9c7d5a8ce8f81afe1ca28224f6
BocchiTheProg <bocchitheprog@gmail.com>
2024-05-29 02:00:27 +0300

    Fix columns references on cast page

--- app/controllers/casts_controller.rb
+++ app/controllers/casts_controller.rb
@@ -9,7 +9,7 @@ class CastsController < ApplicationController
   # GET /casts/1 or /casts/1.json
   def show
     @top_films = @cast.films.joins(:ratings).group(:id).order('AVG(ratings.score) DESC').limit(6)
-    @all_films = @cast.films.includes(:genres).order(release_year: :desc)
+    @all_films = @cast.films.includes(:genres).order(release_date: :desc)
   end
 
   # GET /casts/new
