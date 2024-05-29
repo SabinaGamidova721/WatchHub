6d31663ce43b5903e73b7018b33e3ca6b08d5db1
Oleksandra <alexandraborovik03@gmail.com>
2024-05-29 15:19:03 +0300

    Update some files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,31 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :show_header
+  before_action :show_footer
+
+  private
+
+  def show_header
+    excluded_paths = [
+      "/",
+      "/users/sign_in",
+      "/user_profiles/new",
+      "/users/password/new",
+      "/users/confirmation"
+    ]
+
+    @show_header = !excluded_paths.include?(request.path)
+  end
+
+  def show_footer
+    excluded_paths = [
+      "/users/sign_in",
+      "/user_profiles/new",
+      "/users/password",
+      "/users/confirmation"
+    ]
+
+    @show_footer = excluded_paths.none? {|path| request.path.include?(path) }
+  end
 end
